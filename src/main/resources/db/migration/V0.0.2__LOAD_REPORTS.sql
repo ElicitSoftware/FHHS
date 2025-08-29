5cf4ccce9d220709a052500f1e5457f2b86c2260
Matthew Demerath <mdemerat@med.umich.edu>
2025-08-28 22:00:20 -0400

    Fixed the sequence name

--- src/main/resources/db/migration/V0.0.2__LOAD_REPORTS.sql
+++ src/main/resources/db/migration/V0.0.2__LOAD_REPORTS.sql
@@ -9,9 +9,9 @@
 -- ***LICENSE_END***
 ---
 -- Test Reports --
-INSERT INTO survey.reports(id, survey_id, name, description, url, display_order) VALUES(nextval('survey.relationships_seq'), 1, 'Respondent', 'Respondent', 'http://host.docker.internal:8082/proband/report', 0);
-INSERT INTO survey.reports(id, survey_id, name, description, url, display_order) VALUES(nextval('survey.relationships_seq'),1, 'Cancer Summary', 'Cancer Summary', 'http://host.docker.internal:8082/casummary/report', 1);
-INSERT INTO survey.reports(id, survey_id, name, description, url, display_order) VALUES(nextval('survey.relationships_seq'),1, 'Family Pedigree', 'Family Pedigree', 'http://host.docker.internal:8082/pedigree/report', 3);
+INSERT INTO survey.reports(id, survey_id, name, description, url, display_order) VALUES(nextval('survey.reports_seq'), 1, 'Respondent', 'Respondent', 'http://host.docker.internal:8082/proband/report', 0);
+INSERT INTO survey.reports(id, survey_id, name, description, url, display_order) VALUES(nextval('survey.reports_seq'),1, 'Cancer Summary', 'Cancer Summary', 'http://host.docker.internal:8082/casummary/report', 1);
+INSERT INTO survey.reports(id, survey_id, name, description, url, display_order) VALUES(nextval('survey.reports_seq'),1, 'Family Pedigree', 'Family Pedigree', 'http://host.docker.internal:8082/pedigree/report', 2);
 -- Post Survey Actions --
 INSERT INTO survey.post_survey_actions (id, survey_id, name, description, url, execution_order)
 VALUES (
